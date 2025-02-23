88888570aa443b5872836f2a28eaf511236a85e2
Thales Fernandes <contato.thales@gmail.com>
2025-02-23 14:34:33 -0300

    Desafios realizados, caracteres do nome e soma

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("arquivo main")
+print("Desafio primeira aula")
+print("_______________")
+
+# Crie um programa que o usuário digita o seu nome e retorna o número de caracteres
+
+print("O numero de caracteres é: ", len(input("Digite o seu nome: ")))
+
+
+
+# Crie um programa que o usuário digite 02 valores e apareça a soma
+
+numero1 = int(input("Digite um numero: "))
+numero2 = int(input("Digite outro numero: "))
+soma = numero1 + numero2
+print("A soma é ", soma)
